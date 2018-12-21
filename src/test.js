af085c20f7e6d095c5203fc99867b9d7512c5f7e
Matt Keeneth <mkeeneth@gmail.com>
2018-12-20 19:45:51 -0500

    Refactor click logic

--- src/test.js
+++ src/test.js
@@ -2,36 +2,30 @@
 import "babel-polyfill";
 import * as C from "./constants";
 
-const waitTime = 80;
+const waitTime = 50;
 const timeout = ms => new Promise(res => setTimeout(res, ms));
 
+const clickAndWait = async element => {
+  element.click();
+  await timeout(waitTime);
+};
+
 export const happyPathTest = async () => {
-  document.getElementById("battleships_list").children[0].click();
-  await timeout(waitTime);
-  document.getElementById("playergrid_1_1").click();
-  await timeout(waitTime);
-  document.getElementById("battleships_list").children[1].click();
-  await timeout(waitTime);
-  document.getElementById("playergrid_2_3").click();
-  await timeout(waitTime);
-  document.getElementById("battleships_list").children[2].click();
-  await timeout(waitTime);
-  document.getElementById("playergrid_4_5").click();
-  await timeout(waitTime);
-  document.getElementById("battleships_list").children[3].click();
-  await timeout(waitTime);
-  document.getElementById("playergrid_6_1").click();
-  await timeout(waitTime);
-  document.getElementById("battleships_list").children[4].click();
-  await timeout(waitTime);
-  document.getElementById("playergrid_2_7").click();
-  await timeout(waitTime);
+  await clickAndWait(document.getElementById("battleships_list").children[0]);
+  await clickAndWait(document.getElementById("playergrid_1_1"));
+  await clickAndWait(document.getElementById("battleships_list").children[1]);
+  await clickAndWait(document.getElementById("playergrid_2_3"));
+  await clickAndWait(document.getElementById("battleships_list").children[2]);
+  await clickAndWait(document.getElementById("playergrid_4_5"));
+  await clickAndWait(document.getElementById("battleships_list").children[3]);
+  await clickAndWait(document.getElementById("playergrid_6_1"));
+  await clickAndWait(document.getElementById("battleships_list").children[4]);
+  await clickAndWait(document.getElementById("playergrid_2_7"));
 
   for (let i = 0; i < C.GRID_SIZE; i++) {
     for (let ii = 0; ii < C.GRID_SIZE; ii++) {
       // click it
-      document.getElementById(C.ENEMY_OCEAN_PREFIX + i + "_" + ii).click();
-      await timeout(waitTime);
+      await clickAndWait(document.getElementById(C.ENEMY_OCEAN_PREFIX + i + "_" + ii));
     }
   }
 };
